1afbbe2280a2ee0214e8252e13d90d5db61aee61
khuongbk <nguyentienkhuong1104@gmail.com>
2020-03-30 18:09:15 +0700

    Update ocr.py

--- ocr_server/ocr.py
+++ ocr_server/ocr.py
@@ -1,6 +1,7 @@
 import cv2 
 import pytesseract
 import configparser
+import json
 from helper import ruttien_helper, chuyentien_helper, tietkiem_helper, tattoan_helper
 
 config = configparser.ConfigParser()
@@ -9,7 +10,7 @@ config.read('ocr.cfg')
 
 def execute_ocr(type_doc="", file_name=""):
     img = cv2.imread(file_name, 0)
-    print(img.shape)
+    # print(img.shape)
     try:
         if config[type_doc]['size_image']:
             size_image = eval(config[type_doc]['size_image'])
@@ -17,12 +18,11 @@ def execute_ocr(type_doc="", file_name=""):
     except Exception as e:
         raise e
     finally:
-        print(img.shape)
-            
+        print(img.shape)            
         ocr_result = {}
         
         for key in config[type_doc]:
-            if(key != "size_image"):
+            if(key != "size_image") :
                 field = eval(config[type_doc][key])
                 #crop
                 y,h,x,w = field[0], field[1], field[2],field[3]
@@ -33,16 +33,15 @@ def execute_ocr(type_doc="", file_name=""):
                 dim = (width, height)
                 resized = cv2.resize(crop_img, dim)
                 if field[6] == 1:
-                    ret, resized = cv2.threshold(resized,160,255,cv2.THRESH_BINARY)
+                    ret, resized = cv2.threshold(resized,200,255,cv2.THRESH_TRUNC)
                 
+                conf = ("--psm 11 --psm 3")
                 result = pytesseract.image_to_string(resized)
                 if (field[7] == 1):
-                    result = pytesseract.image_to_string(resized, lang='vie')
-                
-                print(result)    
+                    conf = ("-l vie --psm 11 --psm 3")
+                    result = pytesseract.image_to_string(resized, config=conf)
+                                   
                 ocr_result[key] = result
-                # cv2.imshow("stk", resized)
-                # cv2.waitKey(0)
 
         if(type_doc == "rut-tien"):
             return ruttien_helper.read_data(ocr_result['currency'],ocr_result['stk'],ocr_result['money'])
@@ -52,7 +51,7 @@ def execute_ocr(type_doc="", file_name=""):
             return tietkiem_helper.read_data(ocr_result['kyhan'],ocr_result['laisuat'],ocr_result['currency'],ocr_result['money'],ocr_result['stks'],ocr_result['stk'])
         elif(type_doc == "tat-toan"):
             return tattoan_helper.read_data(ocr_result['money'],ocr_result['stk'],ocr_result['stt'])       
-    
+   
     
 # for i in range(1,4):
 #     print(execute_ocr(type_doc='tat-toan',file_name="/home/tekai/Desktop/PYTHON/tesseract-python/tat-toan/{}.png".format(i)))
